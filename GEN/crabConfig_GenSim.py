8cd37fb1741b342cdf2bca9de02dc96323dbbe5f
Vladimir <vladimir.cherepanov@cern.ch>
2019-05-09 17:56:39 +0200

    small update

--- GEN/crabConfig_GenSim.py
+++ GEN/crabConfig_GenSim.py
@@ -7,18 +7,17 @@ config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'MinBias_Gen.py'
-#config.JobType.numCores = 8
+config.JobType.psetName = 'MinimumBias_Config.py'
 
-config.Data.outputPrimaryDataset = 'DsTau3Mu'
+config.Data.outputPrimaryDataset = 'MinBias'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 100000 #we expect 50 events per file
-NJOBS = 10 #total number of events = 500000
+config.Data.unitsPerJob = 100000 
+NJOBS = 5
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.inputDBS = 'phys03'
 
 config.Data.outLFNDirBase = '/store/user/cherepan/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'CRAB3_MC2016_MinBias'
+config.Data.outputDatasetTag = 'CRAB3_MC2016_MinBias_SmallScaleTest'
 
 config.Site.storageSite = 'T2_US_Florida'
