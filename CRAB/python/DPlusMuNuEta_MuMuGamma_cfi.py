9e338c07e888e5af94477c9e6b21baa00541fec9
Vladimir <vladimir.cherepanov@cern.ch>
2020-02-13 18:34:31 +0100

    add D+

---
+++ CRAB/python/DPlusMuNuEta_MuMuGamma_cfi.py
@@ -0,0 +1,43 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(14000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'SoftQCD:nonDiffractive = on',
+            'SoftQCD:singleDiffractive = on',
+            'SoftQCD:doubleDiffractive = on',
+            '411:onMode = off',
+            '411:onIfMatch = 13 14 221',
+            '221:onMode = off',
+            '221:onIfMatch = 13 13 22',
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+DPlusFilter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(411)  #D_Plus
+)
+
+MuFilter = cms.EDFilter("MCParticlePairFilter",
+    MinP = cms.untracked.vdouble(2.5, 2.5),
+    MaxEta = cms.untracked.vdouble(3.0, 3.0),
+    MinEta = cms.untracked.vdouble(-3.0, -3.0),
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13)
+)
+
+ProductionFilterSequence = cms.Sequence(generator * DPlusFilter * MuFilter)
