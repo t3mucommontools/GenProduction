0023c74eef24edd9994b5bd104733dd8279bdabb
Luca Guzzi <luca.guzzi@mib.infn.it>
2023-02-22 19:48:39 +0100

    ready for GEN production

--- WTo3MuNu/crab/README.md
+++ WTo3MuNu/crab/README.md
@@ -16,4 +16,5 @@ config.Data.unitsPerJob   = 2000
 config.Data.totalUnits    = 5e+6
 ```
 
-where unitsPerJob **should be identical** to what is written in the .sh file (and has no direct effect), while totalUnits is the total number of events we want. Crab will determine the number of jobs based on this numbers (2500 in this case).
+where unitsPerJob **must be identical** to what is written in the .sh file (and has no direct effect), while totalUnits is the total number of events we want. Crab will determine the number of jobs based on this numbers (2500 in this case). The value nEvents for the externalLHEProducer is overridden by the .sh file. 
+The output of this step is a .root file containing the LHE and GEN information, and a .txt file containing the random seed used for the generation. 
--- WTo3MuNu/crab/crab_LHEGEN.py
+++ WTo3MuNu/crab/crab_LHEGEN.py
@@ -4,26 +4,28 @@ config = config()
 config.General.requestName      = 'W3MuNu_LHEGEN'
 config.General.transferOutputs  = True
 config.General.transferLogs     = True
-config.General.workArea         = 'testLHEGEN'
+config.General.workArea         = 'crabLHEGEN'
 
 config.JobType.pluginName       = 'PrivateMC'
 config.JobType.psetName         = 'pset_LHEGEN.py'
 config.JobType.inputFiles       = [
-                                    'pset_LHEGEN.py',
+                                    'LHEGEN.sh',
                                     'CMSSW_10_6_30_patch1.tar', 
                                     'ppW3MuNu_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz',
                                   ]
 config.JobType.disableAutomaticOutputCollection = True
 config.JobType.numCores         = 4
-config.JobType.maxMemoryMB      = 10000
+config.JobType.maxMemoryMB      = 2499
 config.JobType.maxJobRuntimeMin = 2750
 config.JobType.scriptExe        = 'run_LHE.sh'
 config.JobType.outputFiles      = [
                                     'LHEGEN.root',
+                                    #'SEED.txt',  # don't include when publishingcra
                                   ]
 
 
 config.Data.outputPrimaryDataset = 'WTo3MuNu_ptl0p5_drll0p01_TuneCP5_LO_13TeV_pythia8-madgraph'
+#config.Data.outputPrimaryDataset = 'WTo3MuNu_test4'
 
 # unitsPerJob: number of events per job (keep reading)
 # totalUnits: total number of events
@@ -33,10 +35,10 @@ config.Data.outputPrimaryDataset = 'WTo3MuNu_ptl0p5_drll0p01_TuneCP5_LO_13TeV_py
 # per job is determined inside the pset python file.
 # unitsPerJob value must be identical to that value to have a correct job splitting
 config.Data.splitting     = 'EventBased'
-config.Data.unitsPerJob   = 2e+3
-config.Data.totalUnits    = 5e+6
+config.Data.unitsPerJob   = int(2e+3)
+config.Data.totalUnits    = int(5e+6)
 config.Data.publication   = True
 
 # to be modified
-config.Data.outLFNDirBase = PATH/TO/STORAGE
-config.Site.storageSite = SITE
+config.Data.outLFNDirBase = '/store/user/lguzzi/'
+config.Site.storageSite   = 'T3_IT_MIB'
