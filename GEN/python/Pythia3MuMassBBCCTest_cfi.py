e3e607240eb72b9b4156acc08fdd449935c76941
Arun M Madhu <arunmmadhu@gmail.com>
2025-04-18 16:11:18 +0200

    removed random seeds

--- GEN/python/Pythia3MuMassBBCCTest_cfi.py
+++ GEN/python/Pythia3MuMassBBCCTest_cfi.py
@@ -1,5 +1,4 @@
 import FWCore.ParameterSet.Config as cms
-import random
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
@@ -114,22 +113,6 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     maxEventsToPrint = cms.untracked.int32(1),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     pythiaPylistVerbosity = cms.untracked.int32(1)
-)
-
-seed = random.randint(1, 900000000)
-RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-    generator = cms.PSet(
-        initialSeed = cms.untracked.uint32(seed),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    VtxSmeared = cms.PSet(  # ? add this block
-        initialSeed = cms.untracked.uint32(seed + 1),
-        engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    g4SimHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(seed + 2),
-        engineName = cms.untracked.string('HepJamesRandom')  # or 'TRandom3'
-    )
 )
 
 
